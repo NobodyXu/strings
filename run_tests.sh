239661518d8735e6013667e1a80cbf2881db9985
Jiahao XU <Jiahao_XU@outlook.com>
2021-12-25 01:32:34 +1100

    Add run_tests.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+cd $(dirname `realpath $0`)
+
+export RUST_TEST_THREADS=1
+
+rep=$(seq 1 10)
+
+args="--all-features -- --nocapture"
+
+for _ in $rep; do
+    cargo test $args
+done
+
+export RUSTFLAGS='-Zsanitizer=address'
+export RUSTDOCFLAGS="$RUSTFLAGS"
+for _ in $rep; do
+    cargo +nightly test $args
+done
+
+export MIRIFLAGS="-Zmiri-disable-isolation"
+exec cargo +nightly miri test $args
